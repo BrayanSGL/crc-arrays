8bdaf3eaa8316041780fdefde156564456634f13
Brayan Galeano <bsgaleanol@correo.udistrital.edu.co>
2022-10-19 00:46:58 -0500

    Cerca

--- main.py
+++ main.py
@@ -1,52 +1,33 @@
 from ctypes import Array
 import os
 
-from numpy import array
 
-
-POLYNOMIAL = [
-    {
-        'CRC': 'CRC-32',
-        'code': 32,
-        'generator': 'x32 + x26 + x23 + x22 + x16 + x12 + x11 + x10 + x8 + x7 + x5 + x4 + x2 + x + 1',
-        'hexa': 0x104C11DB7,
-        'binary': [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1]
-    },
-    {'CRC': 'CRC-3',
-        'code': 3,
-        'generator': 'x3 + x2 + 1',
-        'hexa': 0x104C11DB7,
-        'binary': [1, 1, 0, 1]
-     }
-
-]
-
-POLYNOMIAL2 = {
-    'CRC': 'CRC-3',
-    'code': 3,
-    'generator': 'x3 + x2 + 1',
-    'hexa': 0x104C11DB7,
-    'binary': [1, 1, 0, 1]
+POLYNOMIAL = {
+    'CRC': 'CRC-16',
+    'code': 16,
+    'generator': 'P(x) = x^16 + x^15 + x^2 + 1',
+    'hexa': 0x18005,
+    'binary': [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1]
 }
+
+# convertir una arreglo de numeros a string
+
+
+def convertir_a_string(arreglo):
+    string = ''
+    for i in arreglo:
+        string += str(i)
+    return string
 
 
 def xor(a, b):
     return [int(x) ^ int(y) for x, y in zip(a, b)]
 
 
-def calcular_crc(generator: Array, trama: Array) -> Array:
-    binary_of_generator = generator[0].get('binary')
+def calcular_residuo(generator: Array, trama: Array) -> Array:
     while True:
         # Paso 3.1: Obtener el residuo
-        print('----------VUELTA----------')
-        residuo = xor(binary_of_generator, trama[:len(binary_of_generator)])
-        print('generador')
-        print(binary_of_generator)
-        print('trama con el tamaño del generador')
-        print(trama[:len(binary_of_generator)])
-        print('Residuo')
-
-        print(residuo)
+        residuo = xor(generator, trama[:len(generator)])
 
         # Paso 3.2: Quitar los ceros a la izquierda de residuo si existen
         ceros = 0
@@ -55,31 +36,24 @@ def calcular_crc(generator: Array, trama: Array) -> Array:
                 ceros += 1
                 residuo.pop(0)
         except IndexError:
-            return [0] * len(binary_of_generator)
+            return [0] * len(generator)
 
         # Paso 3.3: Recortar la trama la cantidad de ceros a la izquierda
-        print('trama original')
-        print(trama)
-        trama = trama[len(binary_of_generator):]
-        print('Trama recortada: ')
-        print(trama)
+        trama = trama[len(generator):]
 
         trama = residuo + trama
-        print('Trama con residuo: ')
-        print(trama)
 
-
-        # Paso 3.3: Verificar si la trama ya esta dividida
-        if len(trama) < len(binary_of_generator):
+        if len(trama) <= len(generator):
             # agregar a residuo seros a la izquierda hasta que tenga longitud 3
-            while len(residuo) < generator[0].get('code'):
-                residuo.insert(0, 0)
-            return residuo
+            while len(trama) < len(generator)-1:
+                trama.insert(0, 0)
+            return trama
+
 
 def main():
     while True:
         os.system('cls')
-        print(' ------ Calculo de código CRC -------\n1.Ingrese cualquier cadena\n2.Ingrese: @Salir para cerrar el programa\n')
+        print(' ------ Calculo de código CRC -------\n \n1.Ingrese cualquier cadena\n2.Ingrese: @Salir para cerrar el programa\n')
         string = input('# Ingrese la cadena de Texto > ')
         if string == '@Salir':
             print('------Programa terminado por el usuario-----')
@@ -88,24 +62,33 @@ def main():
         # Paso 1: Recibo la trama y la convierto en binario
         binary_string = list(
             map(int, (''.join(format(ord(x), 'b') for x in string))))
-        print('Trama en binario: ', binary_string)
+
+        os.system('cls')
+        print('-----------Resultado----------- \n')
+        print('Texto ingresado: ', string)
+        print('Texto en binario: ', convertir_a_string(binary_string))
+        print('Polinomio generador: ', POLYNOMIAL.get(
+            'CRC'), ' ', POLYNOMIAL.get('generator'))
 
         # Paso 2: Agrego el residuo a la trama
-        for _ in range(len(POLYNOMIAL[0].get('binary'))-1):
+
+        for _ in range(len(POLYNOMIAL.get('binary'))-1):
             binary_string.append(0)
-        print('Trama con residuo: ', binary_string)
+        #print('Trama con residuo: ', binary_string)
 
         # Paso 3 Resolver la división
-        #binary_string = [1, 1, 1, 1, 0, 1, 0, 0, 0]
-        crc = calcular_crc(generator=POLYNOMIAL, trama=binary_string)
-        print('Residuo: ', crc)
+        residuo = calcular_residuo(POLYNOMIAL.get('binary'), binary_string)
+
+        for _ in range(len(POLYNOMIAL.get('binary'))-1):
+            binary_string.pop(-1)
+
+        print('Residuo de la división: ', convertir_a_string(residuo))
+        print('Secuencia binaria de datos para la detección de errores: ',
+              convertir_a_string(binary_string+residuo))
         os.system('pause')
 
-        #Comprobacion de CRC
-        
-        comprobacion = calcular_crc(generator=POLYNOMIAL, trama=binary_string+crc)
-        print('Comprobacion: ', comprobacion)
-        os.system('pause')
+        # Comprobacion de CRC
+
 
 if __name__ == '__main__':
     main()
